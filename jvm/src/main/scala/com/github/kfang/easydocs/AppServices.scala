4b83793f674cc657fc1c7dc07eb91a1639cc886f
kfang <hkr.fang@gmail.com>
2016-02-14 15:42:54 -0800

    finally get elasticsearch to run locally

--- jvm/src/main/scala/com/github/kfang/easydocs/AppServices.scala
+++ jvm/src/main/scala/com/github/kfang/easydocs/AppServices.scala
@@ -3,11 +3,15 @@ package com.github.kfang.easydocs
 import akka.actor.ActorSystem
 import com.sksamuel.elastic4s.ElasticClient
 import com.github.kfang.easydocs.services.{SitesHandler, IndexService}
+import org.elasticsearch.common.settings.Settings
 
 case class AppServices(system: ActorSystem, config: AppConfig) {
 
   //TODO: configure remote elasticsearch client
-  val elasticClient = ElasticClient.local
+  private val esClientSettings = Settings.settingsBuilder()
+    .put("http.enabled", false)
+    .put("path.home", "~/elastic/data")
+  val elasticClient = ElasticClient.local(esClientSettings.build)
   val endpointService = system.actorOf(IndexService.props(elasticClient), "index-service")
 
   SitesHandler.ensureSitesIndex(elasticClient)(system)
